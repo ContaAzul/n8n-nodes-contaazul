4146b9825531c6133a0eb50fde0cf53f99daf176
Kallklen <kallklen@yahoo.com.br>
2025-08-06 09:38:59 -0300

    fix: icon file on build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 const path = require('path');
-const { task, src, dest } = require('gulp');
+const { task, src, dest, parallel } = require('gulp');
 
-task('build:icons', copyIcons);
+task('build:icons', parallel(copyNodeAssetIcons, copyRootNodeIcons, copyIconsFolder));
 
-function copyIcons() {
+function copyNodeAssetIcons() {
   const nodeSource = path.resolve('nodes/assets/contaazul.svg');
   const nodeDestination = path.resolve('dist/nodes/ContaAzul/assets');
 
   return src(nodeSource).pipe(dest(nodeDestination));
+}
+
+function copyRootNodeIcons() {
+  const nodeSource = path.resolve('nodes/ContaAzul/contaazul.svg');
+  const nodeDestination = path.resolve('dist/nodes/ContaAzul');
+
+  return src(nodeSource).pipe(dest(nodeDestination));
+}
+
+function copyIconsFolder() {
+  const iconSource = path.resolve('icons/**/*.svg');
+  const iconDestination = path.resolve('dist/icons');
+
+  return src(iconSource).pipe(dest(iconDestination));
 }
